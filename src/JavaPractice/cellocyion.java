880ea21bc340599714361e2dcc1dd222984d31a6
Yusan410 <yissan517@gmail.com>
2022-05-29 22:24:51 -0400

    NewClassAded

---
+++ src/JavaPractice/cellocyion.java
@@ -0,0 +1,40 @@
+package JavaPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class cellocyion {
+
+	public static void main(String[] args) {
+
+		Integer num[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 16, 17 };
+
+		ArrayList<Integer> list = new ArrayList<>(Arrays.asList(num));
+
+		ArrayList<Integer> DuplicateElement = new ArrayList<>();
+
+		ArrayList<Integer> nonDuplicate = new ArrayList<>();
+
+		for (Integer a : list) {
+
+			if (Collections.frequency(list, a) > 1) {
+
+				DuplicateElement.add(a);
+
+			}
+
+			if (Collections.frequency(list, a) == 1) {
+
+				nonDuplicate.add(a);
+
+			}
+		}
+
+		System.out.println(nonDuplicate);
+
+		System.out.println(DuplicateElement);
+
+	}
+
+}
